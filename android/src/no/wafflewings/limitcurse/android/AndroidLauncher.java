ec4afcfbfcc0e7187a61b07f62ff832a04401ca3
Karl <bolla_nor@hotmail.com>
2014-10-24 20:46:42 +0200

    initial libgdx project

---
+++ android/src/no/wafflewings/limitcurse/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package no.wafflewings.limitcurse.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import no.wafflewings.limitcurse.LimitCurse;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LimitCurse(), config);
+	}
+}
---
+++ desktop/src/no/wafflewings/limitcurse/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package no.wafflewings.limitcurse.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import no.wafflewings.limitcurse.LimitCurse;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LimitCurse(), config);
+	}
+}
